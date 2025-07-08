541054729e2f3202d83ea4ae43f194ee4786d354
rhv1501 <rhv4748@gmail.com>
2025-07-08 12:40:34 +0530

    added ui changes

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -93,9 +93,11 @@ export default async function BlogPage({ params }) {
               className="w-[130px] sm:w-auto"
             />
           </Link>
-          <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000]">
-            Get Started <Image src={assets.arrow} alt="an arrow" />
-          </button>
+          <Link href="https://www.rhv1501.me" target="_blank">
+            <button className="cursor-pointer flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000]">
+              visit Author <Image src={assets.arrow} alt="an arrow" />
+            </button>
+          </Link>
         </div>
         <div className="text-center my-24">
           <h1 className="text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto">
--- components/AdminComponents/Sidebar.jsx
+++ components/AdminComponents/Sidebar.jsx
@@ -6,7 +6,9 @@ const Sidebar = () => {
     <>
       <div className="flex-col bg-slate-100 hidden lg:flex">
         <div className="px-2 sm:pl-14 py-3 border border-black">
-          <Image src={assets.logo} alt={"logo"} width={120} />
+          <Link href={"/admin"}>
+            <Image src={assets.logo} alt={"logo"} width={120} />
+          </Link>
         </div>
         <div className="w-28 sm:w-80 h-[100vh] relative py-12 border border-black">
           <div className="w-[50%] sm:w-[80%] absolute right-0">
